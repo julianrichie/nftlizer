dbfb4bb047428154008354fb9b3589ffc22aad3d
Julian Wajong <julian.wajong@gmail.com>
2022-07-25 14:20:08 +0700

    Update TransferableOwnership.sol

--- TransferableOwnership.sol
+++ TransferableOwnership.sol
@@ -4,25 +4,17 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-abstract contract UseProxyContract is AccessControl { 
+abstract contract TransferableOwnership is AccessControl {
 
-    address internal _WithdrawalWalletAddress = 0x0000000000000000000000000000000000000000;
-    address internal _NFTLizerProxyContract = 0x0000000000000000000000000000000000000000;
+    address internal _ContractOwner;
 
-    function setWithdrawalWalletAddress(address addr) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _WithdrawalWalletAddress = addr;
+    function transferOwnership(address from, address to) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _setupRole(DEFAULT_ADMIN_ROLE,to);
+        _ContractOwner = to;
+        _revokeRole(DEFAULT_ADMIN_ROLE,from);
     }
 
-    function setNFTLizerProxyContractAddress(address addr) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _NFTLizerProxyContract = addr;
+    function getContractOwner() public view returns(address) {
+        return _ContractOwner;
     }
-
-    function getWithdrawalWalletAddress() public view returns (address) {
-        return _WithdrawalWalletAddress;
-    }
-
-    function getNFTLizerProxyContractAddress() public view returns (address) {
-        return _NFTLizerProxyContract;
-    }
-
 }

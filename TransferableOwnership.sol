39b4ec85404f2b4cdd1610d738ffcd2afa18aa0a
Julian Wajong <julian.wajong@gmail.com>
2022-08-05 21:30:33 +0700

    Update TransferableOwnership.sol

--- TransferableOwnership.sol
+++ TransferableOwnership.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 abstract contract TransferableOwnership is AccessControl {
 
     address internal _ContractOwner;
-    bool internal _ContractInitialized = false;
 
     function transferOwnership(address from, address to) public onlyRole(DEFAULT_ADMIN_ROLE) {
         _setupRole(DEFAULT_ADMIN_ROLE,to);
@@ -16,7 +15,6 @@ abstract contract TransferableOwnership is AccessControl {
     }
 
     function getContractOwner() public view returns(address) {
-        require(_ContractInitialized,"not initialized");
         return _ContractOwner;
     }
 }

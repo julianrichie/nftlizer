50bdc718495b2fe9d6482401ed201335b1ebe77c
Julian Wajong <julian.wajong@gmail.com>
2022-08-12 16:59:41 +0700

    Update UseProxyContract.sol

--- UseProxyContract.sol
+++ UseProxyContract.sol
@@ -5,32 +5,30 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 abstract contract UseProxyContract is AccessControl {
-     
+
     //DONT FORGET TO OVERRIDE THIS ON INITIALIZATION
-    address internal _WithdrawalWalletAddress = 0x0000000000000000000000000000000000000000;
-    address internal _NFTLizerWithdrawalWalletAddress = 0x0000000000000000000000000000000000000000;
+    address internal _NFTLizerWalletAddress = 0x0000000000000000000000000000000000000000;
     address internal _NFTLizerProxyContract = 0x0000000000000000000000000000000000000000;
 
-    function setWithdrawalWalletAddress(address addr) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _WithdrawalWalletAddress = addr;
+    modifier callerIsProxyContract() {
+        require(_NFTLizerProxyContract == msg.sender,"unauthorized");
+        _;
     }
 
-    function setNFTLizerProxyContractAddress(address addr) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    function setNFTLizerWalletAddress(address addr) public callerIsProxyContract {
+        _NFTLizerWalletAddress = addr;
+    }
+
+    function setNFTLizerProxyContractAddress(address addr) public callerIsProxyContract {
         _NFTLizerProxyContract = addr;
     }
 
-    function getWithdrawalWalletAddress() public view returns (address) {
-        return _WithdrawalWalletAddress;
+    function getNFTLizerWalletAddress() public view returns (address) {
+        return _NFTLizerWalletAddress;
     }
 
     function getNFTLizerProxyContractAddress() public view returns (address) {
         return _NFTLizerProxyContract;
-    }
-
-    function _TransferToken(uint256 amt, address destination) internal returns (bool){
-       address wallet = payable(destination);
-       (bool success,) = wallet.call{value: amt}("");
-       return(success);
     }
 
 }

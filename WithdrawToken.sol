4f8dd9ff937e85492879ba97ac6993e2e0cf7524
Julian Wajong <julian.wajong@gmail.com>
2022-08-12 22:37:15 +0700

    Update WithdrawToken.sol

--- WithdrawToken.sol
+++ WithdrawToken.sol
@@ -3,30 +3,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./Utilities.sol";
 
 abstract contract WithdrawToken is AccessControl {
 
-    address internal _WithdrawalWalletAddress = 0x0000000000000000000000000000000000000000;
-
-    function setWithdrawalWalletAddress(address addr) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _WithdrawalWalletAddress = addr;
-    }
-
-    function getWithdrawalWalletAddress() public view returns (address) {
-        return _WithdrawalWalletAddress;
-    }
-
-    function withdrawToken() public onlyRole(DEFAULT_ADMIN_ROLE) {
-        uint256 amt = address(this).balance;
-        address wallet = payable(_WithdrawalWalletAddress);
-        bool success = _TransferToken(amt,wallet);
+    function withdrawToken(address addr) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        address wallet = payable(addr);
+        bool success = Utilities.TransferToken(Utilities.GetAvailableToken(),wallet);
         require(success,"withdrawal process failed");
-    }
-
-    function _TransferToken(uint256 amt, address destination) internal returns (bool){
-       address wallet = payable(destination);
-       (bool success,) = wallet.call{value: amt}("");
-       return(success);
     }
 
 }

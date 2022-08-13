ce7391c7b62fa57774b460c8209d065d0cfe1828
Julian Wajong <julian.wajong@gmail.com>
2022-08-13 08:40:57 +0700

    Update ProxyContract.sol

--- ProxyContract.sol
+++ ProxyContract.sol
@@ -3,18 +3,42 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "github.com/julianrichie/nftlizer/blob/main/TransferableOwnership.sol";
+import "github.com/julianrichie/nftlizer/blob/main/UseProxyContract.sol";
 
-abstract contract TransferableOwnership is AccessControl {
+contract NFTLizerProxyContract is AccessControl, TransferableOwnership {
 
-    address internal _ContractOwner;
+    mapping(address => uint256) private CONTRACT_MINTING_FEE;
+    address private NFTLIZER_WALLET_ADDRESS = 0x0000000000000000000000000000000000000000;
 
-    function transferOwnership(address from, address to) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _setupRole(DEFAULT_ADMIN_ROLE,to);
-        _ContractOwner = to;
-        _revokeRole(DEFAULT_ADMIN_ROLE,from);
+    modifier callerIsContract() {
+        require(Address.isContract(msg.sender),"not a valid contract address");
+        _;
     }
 
-    function getContractOwner() public view returns(address) {
-        return _ContractOwner;
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
+    
+    function getMintingFee() callerIsContract public view returns (uint256){
+        return CONTRACT_MINTING_FEE[msg.sender];
+    }
+
+    function setMintingFee(address addr,uint256 fee) public onlyRole(DEFAULT_ADMIN_ROLE){
+        CONTRACT_MINTING_FEE[addr] = fee;
+    }
+
+    function setNFTLizerWalletAddress(address addr) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        NFTLIZER_WALLET_ADDRESS = addr;
+    }
+
+    function getNFTLizerWalletAddress() public view returns (address) {
+        return NFTLIZER_WALLET_ADDRESS;
+    }
+
+    function setNFTLizerProxyContractAddressForClient(address addr,address client) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        UseProxyContract(client).setNFTLizerProxyContractAddress(addr);
+    }
+
 }
